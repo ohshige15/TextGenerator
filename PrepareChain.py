4fb6edc336f62bd47111b8e5846d6ddb0316f59e
o-tomox <satoshi.o.xxx@gmail.com>
2014-09-23 23:53:18 +0900

    チェーンを作成してDBに保存するためのファイルを作成

---
+++ PrepareChain.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+u"""
+与えられた文書からマルコフ連鎖のためのチェーン（連鎖）を作成して、
+DBに保存するファイル
+"""
+
+class PrepareChain(object):
+	u"""チェーンを作成してDBに保存するクラス
+	"""
+
+	def __init__(self):
+		u"""初期化メソッド
+		"""
+		pass
+
+
+if __name__ == '__main__':
+	pass
