5d3d6642022456cd25470447f523991117659d2b
o-tomox <satoshi.o.xxx@gmail.com>
2014-09-26 23:17:40 +0900

    仮実装

--- PrepareChain.py
+++ PrepareChain.py
@@ -4,6 +4,9 @@ u"""
 与えられた文書からマルコフ連鎖のためのチェーン（連鎖）を作成して、DBに保存するファイル
 """
 
+from collections import defaultdict
+
+
 class PrepareChain(object):
 	u"""
 	チェーンを作成してDBに保存するクラス
@@ -15,6 +18,60 @@ class PrepareChain(object):
 		@param text チェーンを生成するための文章
 		"""
 		self.text = text
+
+	def do_prepare(self):
+		u"""
+		形態素解析からDB保存まで
+		"""
+		# 長い文章をセンテンス毎に分割
+		sentences = self._divide(self.text)
+
+		# 3つ組の出現回数
+		triplet_freqs = defaultdict(int)
+
+		# センテンス毎に3つ組にする
+		for sentence in sentences:
+			# 形態素解析
+			morphemes = self._morphological_analysis(sentence)
+			# 3つ組をつくる
+			triplets = self._make_triplet(morphemes)
+			# 出現回数を加算
+			for (triplet, n) in triplets:
+				triplet_freqs[triplet] += n
+
+		# DBに保存
+		self._save(triplet_freqs)
+
+	def _divide(self, text):
+		u"""
+		「。」や改行などで区切られた長い文章を一文ずつに分ける
+		@param text 分割前の文章
+		@return 一文ずつの配列
+		"""
+		pass
+
+	def _morphological_analysis(self, sentence):
+		u"""
+		一文を形態素解析する
+		@param sentence 一文
+		@return 形態素で分割された配列
+		"""
+		pass
+
+	def _make_triplet(self, morphemes):
+		u"""
+		形態素解析で分割された配列を、形態素毎に3つ組にしてその出現回数を数える
+		@param morphemes 形態素配列
+		@return 3つ組とその出現回数の辞書 key: 3つ組（タプル） val: 出現回数
+		"""
+		pass
+
+	def _save(self, triplet_freqs):
+		u"""
+		3つ組毎に出現回数をDBに保存
+		@param triplet_freqs 3つ組とその出現回数の辞書 key: 3つ組（タプル） val: 出現回数
+		"""
+		pass
 
 
 if __name__ == '__main__':
