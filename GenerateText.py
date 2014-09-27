0d07b56b248ab4837367d556ebdb7ee7793c6f6b
o-tomox <satoshi.o.xxx@gmail.com>
2014-09-27 20:44:03 +0900

    文章を自動生成するファイルを作成

---
+++ GenerateText.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+u"""
+マルコフ連鎖を用いて適当な文章を自動生成するファイル
+"""
+
+class GenerateText(object):
+    u"""
+    文章生成用クラス
+    """
+
+    def __init__(self, n=5):
+        u"""
+        初期化メソッド
+        @param n いくつの文章を生成するか
+        """
+        self.n = n
+
+    def generate(self):
+        u"""
+        実際に生成する
+        """
+        pass
